889bc850462bcf3671de4d43a2b49b4e84d9f5ef
George Goldberg <grundleborg@googlemail.com>
2009-09-17 13:44:01 +0000

    Add a skeleton KDED Module that will be autoloaded when a KDE session is started.
    svn path=/trunk/playground/network/telepathy-launcher-kded/; revision=1024895

---
+++ kded-telepathy-launcher.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of KDE
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "kded-telepathy-launcher.h"
+
+#include <KDebug>
+#include <KPluginFactory>
+
+K_PLUGIN_FACTORY(TelepathyLauncherFactory, registerPlugin<TelepathyLauncher>(); )
+K_EXPORT_PLUGIN(TelepathyLauncherFactory("telepathy_launcher"))
+
+TelepathyLauncher::TelepathyLauncher(QObject *parent, const QList<QVariant> &)
+  : KDEDModule(parent)
+{
+    kDebug();
+}
+
+TelepathyLauncher::~TelepathyLauncher()
+{
+    kDebug();
+}
+
